0dfb2a6eee8ffb93f1f7622def7fa648840efe5c
Manoj Karthick <manojkarthick@ymail.com>
2021-02-14 09:53:31 -0800

    Update SHA256 val

--- Formula/pqrs.rb
+++ Formula/pqrs.rb
@@ -2,7 +2,7 @@ class Pqrs < Formula
     desc "Apache Parquet command-line tools and utilities"
     homepage "https://github.com/manojkarthick/pqrs"
     url "https://github.com/manojkarthick/homebrew-pqrs/releases/latest/download/pqrs-mac.tar.gz"
-    sha256 "08dd0f4db12998f8706d05b3060dfc247f50ecb70ed90568f2403b38c60e16c3"
+    sha256 "bccd78827d4be46865000b1ecb3c68a07ff2f7dbe2e813ed19924ab54e6cb1f9"
     version "0.1.1"
   
     def install
