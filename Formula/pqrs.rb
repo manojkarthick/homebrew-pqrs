0823d7f42b584a8780dd55d4a0dab2eaf3d0712a
Manoj Karthick <manojkarthick@users.noreply.github.com>
2021-09-03 06:24:31 -0700

    Create release v0.2.0

--- Formula/pqrs.rb
+++ Formula/pqrs.rb
@@ -2,8 +2,8 @@ class Pqrs < Formula
   desc "Apache Parquet command-line tools and utilities"
   homepage "https://github.com/manojkarthick/pqrs"
   url "https://github.com/manojkarthick/homebrew-pqrs/releases/latest/download/pqrs-mac.tar.gz"
-  version "0.1.2"
-  sha256 "4b53293e2c58196bc5ca8525416ebc34b7ed6fd54563a5c08095b5206ad077cd"
+  version "0.2.0"
+  sha256 "ea9af596eed062020dbacd4e48105e3b1a6409eaac47bfd35c34c7e1cf0c2c06"
 
   def install
     bin.install "pqrs"
--- Justfile
+++ Justfile
@@ -1,4 +1,4 @@
-version := "0.1.2"
+version := "0.2.0"
 
 package:
     wget https://github.com/manojkarthick/pqrs/releases/download/v{{version}}/pqrs-macos-amd64 -O pqrs
